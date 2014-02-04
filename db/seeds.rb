1cf617e134d168ae6febe66084015b1fe523b70d
rcrespo9 <rudycrespo@hushmail.com>
2014-02-03 21:13:53 -0500

    seeded database with elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Element.delete_all
+
+require 'json'
+require 'httparty'
+
+elements = JSON.parse HTTParty.get('http://www.pnathan.com/static/elements.json').response.body
+
+i = 0
+while i < 118 do
+		Element.create(name: elements.keys[i], symbol: elements.values[i]["symbol"], atomic_number: elements.values[i]["atomic_number"], atomic_weight: elements.values[i]["atomic_weight"])
+	i += 1
+end
